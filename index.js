a38e4d231192561ab2c1e66c40a4ac5c10ff8baa
justjoshriley <joshriley316@gmail.com>
2021-11-09 13:08:48 -0800

    finished displaying and sorting data

--- index.js
+++ index.js
@@ -1,40 +1,82 @@
-import data from './titanic-passengers.js'
-const titanic = document.querySelector('#titanic')
+import data from './titanic-passengers.js';
 
-// titanic.style.display = 'grid';
-// titanic.style.gridTemplateColumns = 'repeat(20, 30px)';
-// titanic.style.gridGap = '30px';
+const titanic = document.querySelector('#titanic');
 titanic.style.display ='flex';
 titanic.style.flexWrap = 'wrap';
 
-const passengers = data.map(passenger => {
-    return document.createElement('div');
-});
+const passengers = data.map(p => {
+    return document.createElement('div')
+})
 
 passengers.forEach(passenger => {
     titanic.appendChild(passenger)
-});
+})
+
+data.sort((a,b) => {
+    if (a.fields.sex === 'female') {
+        return 1
+    }
+    return -1
+})
+
+data.sort((a,b) => {
+    if (a.fields.survived === 'Yes') {
+        return -1
+    }
+    return 1
+})
+
+data.sort((a,b) => {
+    if (a.fields.embarked < b.fields.embarked) {
+        return -1
+    } else if (a.fields.embarked > b.fields.embarked) {
+        return 1
+    }
+    return 0
+
+})
 
 passengers.forEach((passenger, index) => {
+    passenger.classList.add('passenger');
+    passenger.dataset.id = index;
     passenger.style.width = '20px';
     passenger.style.height = '20px';
-    passenger.style.margin = '10px';
-    if (data[index].fields.sex === 'female') {
-        passenger.style.backgroundColor = 'pink';
-    } else if(data[index].fields.sex === 'male') {
-        passenger.style.backgroundColor = 'black';
-    } 
-    if (data[index].fields.survived == 'No') {
-        passenger.style.borderRadius = '50%';
-        passenger.style.opacity = '0.3';
+    passenger.style.margin = '5px';
+    passenger.style.opacity =  data[index].fields.survived === 'Yes' ? '1.0' : '0.5';
+    passenger.style.borderRadius = data[index].fields.sex === 'female' ? '50%' : '0';
+    passenger.style.border = data[index].fields.pclass === 0 ? '2.5px solid black' : data[index].fields.pclass === 1 ? '2.5px solid green' : data[index].fields.pclass === 2 ? '2.5px solid purple' : data[index].fields.pclass === 3 ? '2.5px solid yellow' : '2.5px solid white';
+    const portColor = { S: 'tomato', C: 'cornflowerblue', Q: 'orange', undefined: 'black'};
+    passenger.style.backgroundColor = portColor[data[index].fields.embarked]
+})
+
+
+const passengerDetails = document.querySelector('#passenger-details');
+
+document.body.addEventListener('mouseover', (e) => {
+    if (e.target.matches('.passenger')) {
+        const id = e.target.dataset.id;
+        passengerDetails.style.display = 'block';
+        passengerDetails.style.position = 'absolute';
+        passengerDetails.style.left = `${e.pageX + 3}px`;
+        passengerDetails.style.top = `${e.pageY + 3}px`;
+        passengerDetails.style.backgroundColor = '#ffff';
+        passengerDetails.style.padding = '1.5%';
+
+        passengerDetails.innerHTML = `
+        <strong>${data[id].fields.name}</strong>
+        <ul>
+            <li>Age: ${data[id].fields.age}</li>
+            <li>Fare: ${data[id].fields.fare}</li>
+            <li>Embarked: ${data[id].fields.embarked}</li>
+            <li>Pclass: ${data[id].fields.pclass}</li>
+            <li>sibsp: ${data[id].fields.sibsp}</li>
+        </ul>
+        `
     }
-    if (data[index].fields.fare > 10) {
-        passenger.style.border = '3px solid blue'
-    } else if (data[index].fields.fare < 10) {
-        passenger.style.border = '3px solid red';
-    }
-    if (data[index].fields.embarked == 'S') {
-        // passenger.style.height = '30px';
-        // passenger.style.width = '30px';
+})
+
+document.body.addEventListener('mouseout', (e) => {
+    if (e.target.matches('.passenger')) {
+        passengerDetails.style.display = 'none';
     }
 })
